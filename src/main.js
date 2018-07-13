1e744ece921b12fc7bf45a00669f940044f7503a
Edwin Martinez <kastille84@gmail.com>
2018-07-13 11:25:38 -0400

    added routes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router';
+
 import App from './App.vue'
+import { routes } from './routes.js';
+  // allow vue to use VueRouter
+Vue.use(VueRouter);
+  //import and configure our routes to work with VueRouter & save in router var
+const router = new VueRouter({
+  mode: 'history',
+  routes: routes
+})
+
 
 new Vue({
   el: '#app',
+  router: router, // add router to Vue instance
   render: h => h(App)
 })
